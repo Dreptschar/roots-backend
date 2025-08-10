99816f5d2696cef90901b521f450fa56691a6ffe
dreptschar <dreptschar@gmail.com>
2025-08-10 18:49:23 +0200

    add test

--- server/server_test.go
+++ server/server_test.go
@@ -1,6 +1,23 @@
 package server
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestServer(t *testing.T) {
+	t.Run("Status endpoint returns status", func(t *testing.T) {
+		server := NewRootsServer()
+
+		req, _ := http.NewRequest("GET", "/status", nil)
+		resp := httptest.NewRecorder()
+		server.ServeHTTP(resp, req)
+
+		want := "STATUS"
+		got := resp.Body.String()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
